9cdef99f98c497611c23cc04e1ebd91a93b4aba8
henricopuchetti <84693775+henricopuchetti@users.noreply.github.com>
2022-06-20 10:13:31 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,27 @@
+class lanche {
+    constructor(pao, carne, frango, queijo, cebola, alface, molho, bebida, picles, acompanhamento) {
+        this.pao = pao;
+        this.carne = carne;
+        this.queijo = queijo;
+        this.molho = molho;
+        this.bebida = bebida;
+        this.frango = frango;
+        this.alface = alface;
+        this.cebola = cebola;
+        this.picles = picles;
+        this.acompanhamento = acompanhamento;
+      } 
+    } var combo_alegre = "batata pequena"
+      var combo_big = "batata média e bebida"
+      var combo_hen = "batata média e bebida"
+      
+      const alegre = new lanche("carne","queijo","pao", "batata e fruta" ,combo_alegre); 
+      const big = new lanche("duas carnes", "alface", "queijo", "molho", "cebola", "picles", "pao", combo_big); 
+      const hen = new lanche("frango", "alface", "queijo", "picles", combo_hen);
+      console.log(lanche);
+      
+
+
+
+
+
